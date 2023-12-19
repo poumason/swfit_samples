14e85e5efdfc0dfe014dcd0fc260cd97eef201e8
Pou Lin <poumason@live.com>
2023-12-19 14:48:21 +0800

    add create siri shotcut

--- SOPFinder/SOPFinder/ContentView.swift
+++ SOPFinder/SOPFinder/ContentView.swift
@@ -10,15 +10,46 @@ import Intents
 
 struct ContentView: View {
     @State var result: String = ""
+    @State private var isSiriShortcutPresented = false
     
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
+        NavigationView{
             
-            Text(result.isEmpty ? "Hello World!":"from Siri result")
-            Text(result)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                
+                Text(result.isEmpty ? "Hello World!":"from Siri result")
+                    .font(.title2)
+                    .padding(.vertical, 30)
+                Text(result)
+                    .font(.title)
+                Spacer()
+                Button {
+                    isSiriShortcutPresented = true
+                } label: {
+                    HStack(spacing: 0) {
+                        Image("siriIcon")
+                            .resizable()
+                            .frame(width: 50, height: 50)
+                        Text("Add to Siri")
+                            .font(.title2)
+                            .padding()
+                            .foregroundColor(.white)
+                    }
+                    .padding(.all, 10)
+                    .background(
+                        RoundedRectangle(cornerRadius: 15)
+                            .fill(Color.black)
+                    )
+                }
+                Spacer()
+            }
+            //                .navigationTitle("Siri Shortcuts")
+        }
+        .sheet(isPresented: $isSiriShortcutPresented) {
+            SiriShortcutSetupView(isPresented: $isSiriShortcutPresented)
         }
         .padding()
         .onContinueUserActivity("FindSOPIntent", perform: { userActivity in
---
+++ SOPFinder/SOPFinder/SiriShortcutSetupView.swift
@@ -0,0 +1,60 @@
+//
+//  SiriShortcutSetupView.swift
+//  SOPFinder
+//
+//  Created by POU LIN on 2023/12/19.
+//
+
+import Foundation
+import SwiftUI
+import IntentsUI
+
+// Represents the Siri Shortcut setup view.
+struct SiriShortcutSetupView: UIViewControllerRepresentable {
+    
+    // MARK: - Variables -
+    @Binding var isPresented: Bool
+    
+    // Creates the Siri Shortcut setup view controller.
+    func makeUIViewController(context: Context) -> INUIAddVoiceShortcutViewController {
+        let intent = FindSOPIntent()
+        intent.suggestedInvocationPhrase = "Find SOP"
+        
+        let shortcut = INShortcut(intent: intent)!
+        let vc = INUIAddVoiceShortcutViewController(shortcut: shortcut)
+        vc.delegate = context.coordinator
+        return vc
+    }
+    
+    // Updates the Siri Shortcut setup view controller if needed.
+    func updateUIViewController(_ uiViewController: INUIAddVoiceShortcutViewController, context: Context) {
+        // Update the view controller if needed
+    }
+    
+    // Creates a coordinator to handle interactions with the Siri Shortcut setup view.
+    func makeCoordinator() -> Coordinator {
+        Coordinator(isPresented: $isPresented)
+    }
+    
+    // Coordinator to handle interactions with the Siri Shortcut setup view.
+    class Coordinator: NSObject, INUIAddVoiceShortcutViewControllerDelegate {
+        @Binding var isPresented: Bool
+        
+        init(isPresented: Binding<Bool>) {
+            _isPresented = isPresented
+        }
+        
+        // Called when adding a voice shortcut is completed.
+        func addVoiceShortcutViewController(_ controller: INUIAddVoiceShortcutViewController, didFinishWith voiceShortcut: INVoiceShortcut?, error: Error?) {
+            // Handle the completion
+            isPresented = false
+        }
+        
+        // Called when adding a voice shortcut is canceled.
+        func addVoiceShortcutViewControllerDidCancel(_ controller: INUIAddVoiceShortcutViewController) {
+            // Handle cancellation
+            isPresented = false
+        }
+    }
+    
+}
