cabebfa4c4757b82f74c71cd883ba3acfa0b315b
Pou Lin <poumason@live.com>
2024-01-10 23:02:32 +0800

    add test code/

--- wakeupApp/wakeupApp/MyWebViewUI.swift
+++ wakeupApp/wakeupApp/MyWebViewUI.swift
@@ -10,13 +10,30 @@ import WebKit
 struct MyWebView: UIViewRepresentable {
  
     var url: URL
+    
+    private let delgate: MyWKNavigation = MyWKNavigation()
  
     func makeUIView(context: Context) -> WKWebView {
         return WKWebView()
     }
  
     func updateUIView(_ webView: WKWebView, context: Context) {
+        webView.configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+        webView.navigationDelegate = self.delgate
         let request = URLRequest(url: url)
         webView.load(request)
     }
 }
+
+class MyWKNavigation: NSObject, WKNavigationDelegate {
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction) async -> WKNavigationActionPolicy {
+        return WKNavigationActionPolicy.allow
+    }
+    
+    func webView(_ webView: WKWebView, didReceiveServerRedirectForProvisionalNavigation navigation: WKNavigation!) {
+        print(navigation ?? "")
+    }
+}
+
+// https://www.avanderlee.com/swiftui/sfsafariviewcontroller-open-webpages-in-app/
+//
