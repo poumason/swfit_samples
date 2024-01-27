7e34bbc42c85a7410e5cc9c8cb1b6b5cd91a5514
Pou Lin <poumason@live.com>
2024-01-27 13:27:06 +0800

    using TabView to recog layout

--- vision_kit_app/vision_kit_app/ContentView.swift
+++ vision_kit_app/vision_kit_app/ContentView.swift
@@ -9,29 +9,13 @@ import SwiftUI
 import VisionKit
 
 struct ContentView: View {
-    @State var isShowingScanner = true
-    @State private var scannedText = ""
-    
     var body: some View {
         VStack {
-            if DataScannerViewController.isAvailable && DataScannerViewController.isSupported {
-                ZStack {
-                    DataScannerRepresentable(shouldStartScanning: $isShowingScanner, scannedText: $scannedText)
-                    Text(scannedText)
-                        .padding()
-                        .foregroundColor(.black)
-                        .background(Color.white)
-                }
-            } else {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundStyle(.tint)
-                Text("Hello, world!")
-                if !DataScannerViewController.isSupported {
-                    Text("It looks like this device doesn't support the DataScannerViewController")
-                } else {
-                    Text("It appears your camera may not be available")
-                }
+            TabView {
+                DataScannerView()
+                    .tabItem {
+                        Label("Data Scanner", systemImage: "qrcode.viewfinder")
+                    }
             }
         }
         .padding()
